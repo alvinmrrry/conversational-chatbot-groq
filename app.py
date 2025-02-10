32ffebee47f6e5ece1adafea453bddc552dcc3ef
alvinmrrry <alvin_mrrry@outlook.com>
2025-02-10 17:25:16 +0800

    app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def main():
     system_prompt = st.sidebar.text_input("System prompt:")
     model = st.sidebar.selectbox(
         'Choose a model',
-        [ 'Llama 3.3 70B Versatile', 'llama-3.1-8b-instant', 'llama3-70b-8192', 'llama3-8b-8192']
+        [ 'Llama-3.3-70B-Instruct', 'llama-3.1-8b-instant', 'llama3-70b-8192', 'llama3-8b-8192']
     )
     conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value = 5)
 
